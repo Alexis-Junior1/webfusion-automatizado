1aafb221031f6200807e516eb6740315127d594f
CristianAlexis-tech <cristianaula25m@gmail.com>
2026-05-05 10:07:48 +0200

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>";
+echo "<h1>Bienvenidos a WebFusion Digital</h1>";
+echo "<p>Versión 1.0 - Despliegue Automatizado Inicial</p>";
+echo "</div>";
+?>
