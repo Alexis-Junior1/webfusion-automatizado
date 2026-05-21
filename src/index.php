0928f5b1822bc1f4b7bea6cd4a40fe8bc27a6690
Alexis-Junior1 <alexis.hernandezrodriguez@educa.madrid.org>
2026-05-21 11:42:36 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 echo "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>";
 echo "<h1>Bienvenidos a WebFusion Digital</h1>";
+echo "<h1>Esto es una modificacion</h1>";
 echo "<p>Versión 1.0 - Despliegue Automatizado Inicial</p>";
 echo "</div>";
 ?>
